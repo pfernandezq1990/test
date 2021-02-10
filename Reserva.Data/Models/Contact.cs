8f210a048a57ac44e33a36914a993d45ee2cff38
Pablo Fernández <pablo.fernandez@desoft.cu>
2021-02-10 10:09:02 -0500

    commit 2

--- Reserva.Data/Models/Contact.cs
+++ Reserva.Data/Models/Contact.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace Reserva.Data.Models {
@@ -7,12 +8,18 @@ namespace Reserva.Data.Models {
     public class Contact{
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(25, MinimumLength = 3 )]
         public string Name { get; set; }
 
+        [Required]
         public string ContactType { get; set;}
         
+        [Required]
         public int Phone { get; set; }
 
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime Birthdate { get; set; }
         public virtual ICollection<Reservation> Bookings { get; set; }
 
--- Reserva.Data/Models/Reservation.cs
+++ Reserva.Data/Models/Reservation.cs
@@ -1,14 +1,28 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace Reserva.Data.Models{
     public class Reservation {
         public int Id { get; set;}        
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "0:dd-MM", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
+
+        [Range(1, 5)]
+        [Required]
         public int Ranking { get; set; }
+
+        [Required]
         public bool Favorite { get; set; }
 
+        [Required]
         public int ContactId { get; set; }
 
         [JsonIgnore]
